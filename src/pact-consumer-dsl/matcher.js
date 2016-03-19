56186b3e3c603b9612ad182030953aad67c12f80
Tarcio <tarcio@gmail.com>
2016-03-19 18:54:28 +1100

    Adding matchers

--- src/pact-consumer-dsl/matcher.js
+++ src/pact-consumer-dsl/matcher.js
@@ -1,6 +1,8 @@
 'use strict'
 
 import isNil from 'lodash.isnil'
+import isFunction from 'lodash.isfunction'
+import isUndefined from 'lodash.isundefined'
 
 export function term ({ generate, matcher }) {
   if (isNil(generate) || isNil(matcher)) {
@@ -20,24 +22,24 @@ export function term ({ generate, matcher }) {
   }
 }
 
-export function eachLike (content, { min }) {
-  if(isNil(content)) {
+export function eachLike (content, opts) {
+  if(isUndefined(content)) {
     throw new Error('Error creating a Pact eachLike. Please provide a content argument');
   }
 
-  if(isNil(min) || min <= 1) {
+  if(opts && (isNil(opts.min) || opts.min < 1)) {
     throw new Error('Error creating a Pact eachLike. Please provide opts.min that is > 1');
   }
 
   return {
     'json_class': 'Pact::ArrayLike',
     'contents': content,
-    'min': min
+    'min': isUndefined(opts) ? 1 : opts.min
   }
 }
 
 export function somethingLike (value) {
-  if (isNil(value)) {
+  if (isNil(value) || isFunction(value)) {
     throw new Error('Error creating a Pact somethingLike Match. Value cannot be a function or undefined');
   }
 
