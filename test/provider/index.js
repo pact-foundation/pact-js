2fab73c74b56ca2fffdca2006fe8eb3bf83b2a02
Tarcio <tarcio@gmail.com>
2016-06-11 09:01:48 +1000

    Removing cors dependency

--- test/provider/index.js
+++ test/provider/index.js
@@ -1,10 +1,8 @@
-import cors from 'cors'
 import express from 'express'
 import bodyParser from 'body-parser'
 
 const server = express()
 
-server.use(cors())
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
 
