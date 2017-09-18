b92572492783d68b2e0010d1e27b05a1e23a7dda
Peter Törnhult <peter.tornhult@assaabloy.com>
2017-09-18 12:43:31 +0200

    Add test for non-empty array

--- examples/jest/__tests__/schroedinger.spec.js
+++ examples/jest/__tests__/schroedinger.spec.js
@@ -19,28 +19,72 @@ describe("Schroedinger's API", () => {
     provider: 'MyProvider'
   })
 
-  const EXPECTED_BODY = {
-    cats: eachLike({
-      cat: {
-        name: like('Erwin'),
-        observed: term({
-          generate: 'alive',
-          matcher: 'alive|dead'
-        }),
-      }
-    }, {min: 0}),
-  }
-
   beforeAll(() => provider.setup())
 
   afterAll(() => provider.finalize())
 
-  describe("works", () => {
+  describe("no cats works", () => {
+    const EXPECTED_BODY = {
+      cats: eachLike({
+        cat: {
+          name: like('Erwin'),
+          observed: term({
+            generate: 'alive',
+            matcher: 'alive|dead'
+          }),
+        }
+      }, {min: 0}),
+    }
 
     beforeAll(done => {
       const interaction = {
-        state: 'i have a list of projects',
-        uponReceiving: 'a request for projects',
+        state: 'schroedinger has an empty box',
+        uponReceiving: 'a request for schroedingers cats',
+        withRequest: {
+          method: 'GET',
+          path: '/schroedinger/cats',
+          headers: { 'Accept': 'application/json' }
+        },
+        willRespondWith: {
+          status: 200,
+          headers: { 'Content-Type': 'application/json' },
+          body: EXPECTED_BODY
+        }
+      }
+      provider.addInteraction(interaction).then(done, done)
+    })
+
+    // add expectations
+    it('returns a sucessful body', done => {
+      return getMeSchroedingersCats({ url, port })
+        .then(response => {
+          expect(response.headers['content-type']).toEqual('application/json')
+          expect(response.status).toEqual(200)
+          done()
+        })
+    })
+
+    // verify with Pact, and reset expectations
+    it('successfully verifies', () => provider.verify())
+  })
+
+  describe("one or more cats works", () => {
+    const EXPECTED_BODY = {
+      cats: eachLike({
+        cat: {
+          name: like('Erwin'),
+          observed: term({
+            generate: 'alive',
+            matcher: 'alive|dead'
+          }),
+        }
+      }, {min: 1}),
+    }
+
+    beforeAll(done => {
+      const interaction = {
+        state: 'schroedinger has put a cat in the box',
+        uponReceiving: 'a request for schroedingers cats',
         withRequest: {
           method: 'GET',
           path: '/schroedinger/cats',
