192f9e48dae79a893811d3b64bf90297fb576332
Liran Tal <liran.tal@gmail.com>
2017-11-27 23:43:18 +0200

    fix(build): fix for bash script and output helpful information during projects tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,12 +5,13 @@ npm run dist
 # Only run E2E examples on Node 6+
 if [[ "${TRAVIS_NODE_VERSION}" =~ ^[6-8] ]]; then
   echo "Running e2e examples build for node version ${TRAVIS_NODE_VERSION}"
-  for i in examples/* ; do
+  for i in examples/*; do
+    echo "------------------------------------------------"
+    echo "------------> continuing to test example project: $i"
     cd "$i"
     npm install
     npm test
     cd ../../
-    fi
   done
 else
   echo "Skipping examples for node version ${TRAVIS_NODE_VERSION}"
