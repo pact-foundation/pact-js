2f0a4a80c0540990cf1e4c6ba469de64c6fc0d97
Matt Fellows <matt.fellows@onegeek.com.au>
2017-12-03 11:06:35 +1100

    chore(build): run all examples as part of build #128

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -e
+
 npm run dist
 
 # Only run E2E examples on Node 6+
 if [[ "${TRAVIS_NODE_VERSION}" =~ ^[6-8] ]]; then
-  echo "Running e2e and other examples build for node version ${TRAVIS_NODE_VERSION}"
-  npm run test:examples
+  echo "Running e2e examples build for node version ${TRAVIS_NODE_VERSION}"
+  for i in examples/*; do
+    echo "------------------------------------------------"
+    echo "------------> continuing to test example project: $i"
+    cd "$i"
+    npm install
+    npm test
+    cd ../../
+  done
 else
   echo "Skipping examples for node version ${TRAVIS_NODE_VERSION}"
 fi
