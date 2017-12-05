d7a7eb7e26b0745771723b8f66a2dad093773991
Matt Fellows <matt.fellows@onegeek.com.au>
2017-12-05 13:48:08 +1100

    chore(examples): update e2e example with isodate matching

--- examples/e2e/test/consumer.spec.js
+++ examples/e2e/test/consumer.spec.js
@@ -21,11 +21,17 @@ describe('Pact', () => {
   })
 
   // Alias flexible matchers for simplicity
-  const { somethingLike: like, term, eachLike } = pact
+  const {
+    somethingLike: like,
+    term,
+    eachLike,
+    iso8601DateTimeWithMillis
+  } = pact
 
   // Animal we want to match :)
   const suitor = {
     'id': 2,
+    'available_from': '2017-12-04T14:47:18.582Z',
     'first_name': 'Nanny',
     'animal': 'goat',
     'last_name': 'Doe',
@@ -56,11 +62,7 @@ describe('Pact', () => {
   // API we don't care about
   const animalBodyExpectation = {
     'id': like(1),
-    'date': term({
-      // Testing ISO8601 date time regex
-      matcher: '^([\\+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24\\:?00)([\\.,]\\d+(?!:))?)?(\\17[0-5]\\d([\\.,]\\d+)?)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?$',
-      generate: '2017-12-04T14:47:18.582Z'
-    }),
+    'available_from': iso8601DateTimeWithMillis(),
     'first_name': like('Billy'),
     'last_name': like('Goat'),
     'animal': like('goat'),
