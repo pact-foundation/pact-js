c6b2bfd4b57239e2d611143b6064c1049372ae5b
Michel Boudreau <michelboudreau@gmail.com>
2018-02-20 22:35:53 +1100

    trying to fix memory issue

--- src/common/request.ts
+++ src/common/request.ts
@@ -4,7 +4,6 @@ require("es6-object-assign").polyfill();
 
 import * as Popsicle from "popsicle/dist/common";
 import {Response} from "popsicle/dist/response";
-import {logger} from "./logger";
 
 export enum HTTPMethod {
   GET = "GET",
@@ -17,6 +16,10 @@ export enum HTTPMethod {
 }
 
 export class Request {
+  private readonly transport = Popsicle.createTransport({
+    rejectUnauthorized: false, // Need to tell node to ignore bad ssl cert
+    type: "text",
+  });
   public send(method: HTTPMethod, url: string, body?: string): Promise<string> {
     const opts = {
       body,
@@ -26,22 +29,15 @@ export class Request {
       },
       method,
       timeout: 10000,
-      transport: Popsicle.createTransport({
-        rejectUnauthorized: false, // Need to tell node to ignore bad ssl cert
-        type: "text",
-      }),
+      transport: this.transport,
       url,
     };
-
-    logger.info(`Sending request with opts: ${JSON.stringify(opts)}`);
 
     return Popsicle.request(opts)
       .then((res: Response) => {
         if (res.status >= 200 && res.status < 400) {
-          logger.info(`Resolving promise with: ${res.body}`);
           return res.body;
         } else {
-          logger.info(`Rejecting promise with: ${res.body}`);
           return Promise.reject(res.body);
         }
       });
