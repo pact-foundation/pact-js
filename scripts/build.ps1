2fca47ff5d827fa7eec77ee3062fa98c53f5ef1f
Matt Fellows <matt.fellows@onegeek.com.au>
2018-10-08 12:26:57 +1100

    chore(build): make PoSH handle errors, like, properly

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -7,6 +7,7 @@ if ("$env:nodejs_version" -match "^(6|7|8|9)|1[0-9]+(\.)?.*") {
     cd $_.FullName
     npm i
     npm t
+    if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
   }
 } else {
   echo "Skipping examples for older node versions"
