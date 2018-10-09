c778880a1b379d6ad0e9aeb8a3695b4053de7161
Matt Fellows <matt.fellows@onegeek.com.au>
2018-10-09 11:17:10 +1100

    chore(deprecate): deprecate Node version < 6
    BREAKING CHANGE: No longer supporting Node versions 4 or 5.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,14 +1,9 @@
 npm run dist
 
-if ("$env:nodejs_version" -match "^(6|7|8|9)|[1-9][0-9]+(\.)?.*") {
-  cd c:\pact-js
-  Get-ChildItem ".\examples" -Directory | ForEach-Object {
-    echo "Running examples in $($_.FullName)"
-    cd $_.FullName
-    npm i
-    npm t
-    if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-  }
-} else {
-  echo "Skipping examples for older node versions"
+Get-ChildItem ".\examples" -Directory | ForEach-Object {
+  Write-Output "Running examples in $($_.FullName)"
+  cd $_.FullName
+  npm i
+  npm t
+  if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,13 @@
 
 npm run dist
 
-# Only run E2E examples on Node 6+
-if [[ "${TRAVIS_NODE_VERSION}" =~ ^(6|7|8|9)|[1-9][0-9]+ ]]; then
-  echo "Running e2e examples build for node version ${TRAVIS_NODE_VERSION}"
-  for i in examples/*; do
-    echo "------------------------------------------------"
-    echo "------------> continuing to test example project: $i"
-    cd "$i"
-    npm install
-    npm test
-    cd ../../
-  done
-else
-  echo "Skipping examples for node version ${TRAVIS_NODE_VERSION}"
-fi
+echo "Running e2e examples build for node version ${TRAVIS_NODE_VERSION}"
+for i in examples/*; do
+  echo "------------------------------------------------"
+  echo "------------> continuing to test example project: $i"
+  cd "$i"
+  npm install
+  npm test
+  cd ../../
+done
+
