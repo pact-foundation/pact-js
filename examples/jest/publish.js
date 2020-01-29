b4780efa7a6aebb3b361fe86defb0068ff343383
Timothy Jones <timothy.l.jones@gmail.com>
2020-01-29 11:21:03 +1100

    chore(examples): remove redundant property

--- examples/jest/publish.js
+++ examples/jest/publish.js
@@ -2,7 +2,6 @@ let publisher = require("@pact-foundation/pact-node")
 let path = require("path")
 
 let opts = {
-  providerBaseUrl: "http://localhost:8080",
   pactFilesOrDirs: [path.resolve(process.cwd(), "pacts")],
   pactBroker: "https://localhost:3000",
   pactBrokerUsername: process.env.PACT_USERNAME,
