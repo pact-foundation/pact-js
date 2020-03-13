14f9a2841a706e998b401d1c3337af9ac0ac7ca1
Ronald Holshausen <rholshausen@dius.com.au>
2020-03-13 13:56:52 +1100

    chore: try npm link and neon build with appveyor buikd

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,8 +1,11 @@
 npm run dist
+npm link
+npm run build:v3
 
 Get-ChildItem ".\examples" -Directory | ForEach-Object {
   Write-Output "Running examples in $($_.FullName)"
   cd $_.FullName
+  npm link @pact-foundation/pact
   npm i
   npm t
   if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
