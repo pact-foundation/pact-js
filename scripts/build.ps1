c0b93ff261233fa9818f5c6d8dccad6fd2c63907
Ronald Holshausen <uglyog@gmail.com>
2020-03-17 10:27:10 +1100

    chore: skip the v3 example directory in appveyor

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,9 +4,11 @@ npm run build:v3
 
 Get-ChildItem ".\examples" -Directory | ForEach-Object {
   Write-Output "Running examples in $($_.FullName)"
-  cd $_.FullName
-  npm link @pact-foundation/pact
-  npm i
-  npm t
-  if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+  if ($_.FullName -ne "v3") {
+    cd $_.FullName
+    npm link @pact-foundation/pact
+    npm i
+    npm t
+    if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+  }
 }
