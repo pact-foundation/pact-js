22e115a50c5245626cb3a341207762b978a00acd
Ronald Holshausen <uglyog@gmail.com>
2020-03-17 14:33:36 +1100

    chore: use poshd/popd in Appveyor to keep the current directory

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -5,19 +5,21 @@ npm run build:v3
 Get-ChildItem ".\examples" -Directory | ForEach-Object {
   Write-Output "Running examples in $($_.Name)"
   if ($_.Name -ne "v3") {
-    cd $_.FullName
+    pushd $_.FullName
     npm link @pact-foundation/pact
     npm i
     npm t
     if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+    popd
   }
 }
 
 Get-ChildItem ".\examples\v3" -Directory | ForEach-Object {
   Write-Output "Running V3 examples in $($_.Name)"
-  cd $_.FullName
+  pushd $_.FullName
   npm i
   npm link @pact-foundation/pact
   npm t
   if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+  popd
 }
