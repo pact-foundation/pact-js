d2b8514e93551e2b73312d3cce35c6018f98e2ee
Ronald Holshausen <uglyog@gmail.com>
2020-03-17 15:00:34 +1100

    chore: disabling V3 tests on Appveyor for now

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -14,12 +14,12 @@ Get-ChildItem ".\examples" -Directory | ForEach-Object {
   }
 }
 
-Get-ChildItem ".\examples\v3" -Directory | ForEach-Object {
-  Write-Output "Running V3 examples in $($_.Name)"
-  pushd $_.FullName
-  npm i
-  npm link @pact-foundation/pact
-  npm t
-  if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-  popd
-}
+# Get-ChildItem ".\examples\v3" -Directory | ForEach-Object {
+#   Write-Output "Running V3 examples in $($_.Name)"
+#   pushd $_.FullName
+#   npm link @pact-foundation/pact
+#   npm i
+#   npm t
+#   if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+#   popd
+# }
