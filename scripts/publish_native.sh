a7f06c402e7d340b873be60b42d87f9a161d421f
Ronald Holshausen <uglyog@gmail.com>
2020-03-17 16:17:14 +1100

    chore: need to build native lib before the upload

--- scripts/publish_native.sh
+++ scripts/publish_native.sh
@@ -7,4 +7,5 @@ NODE_VERSION=$(node --version)
 echo "--> Releasing native library for version ${VERSION} and Node version ${NODE_VERSION}"
 
 npm install node-pre-gyp node-pre-gyp-github
+npm run build:v3
 npm run upload-binary
