f4b8e167bf2f012fdea757ee45834e2b4df30583
Ronald Holshausen <uglyog@gmail.com>
2020-03-17 13:55:15 +1100

    chore: add npm deploy step to Appveyor

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,5 +1,6 @@
 if ($APPVEYOR_REPO_TAG -eq 'true') {
   Write-Output "Running deploy (APPVEYOR_REPO_TAG) is true"
+  npm run upload-binary
 } else {
   Write-Output "Skipping deploy (APPVEYOR_REPO_TAG) is false"
 }
