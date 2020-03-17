fb6addd5ef8af5fab6294e0b4216f02550869668
Ronald Holshausen <uglyog@gmail.com>
2020-03-17 13:47:46 +1100

    chore: run the V3 tests on Appveyor

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -12,3 +12,12 @@ Get-ChildItem ".\examples" -Directory | ForEach-Object {
     if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
   }
 }
+
+Get-ChildItem ".\examples\v3" -Directory | ForEach-Object {
+  Write-Output "Running V3 examples in $($_.Name)"
+  cd $_.FullName
+  npm i
+  npm link @pact-foundation/pact
+  npm t
+  if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+}
