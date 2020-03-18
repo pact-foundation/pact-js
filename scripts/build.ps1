10a4e8dd444e23d6f823c0dffd00398cff20e13a
Ronald Holshausen <uglyog@gmail.com>
2020-03-18 15:39:48 +1100

    chore: debugging build on Appveyor

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,5 @@
+Set-PSDebug -Trace 1
+del native\index.node
 npm run dist
 npm run build:v3
 npm link
@@ -19,8 +21,11 @@ Write-Output "Done with E2E tests"
 Get-ChildItem ".\examples\v3" -Directory | ForEach-Object {
   Write-Output "Running V3 examples in $($_.Name)"
   pushd $_.FullName
-  npm link @pact-foundation/pact
   npm i
+  del node_modules\@pact-foundation\pact\native\index.node
+  npm link @pact-foundation/pact
+  dir node_modules
+  dir node_modules/@pact-foundation/pact
   npm t
   if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
   popd
