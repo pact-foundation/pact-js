6795c7e7b3eb9b7482225bc60db936859c918070
Ronald Holshausen <uglyog@gmail.com>
2020-03-20 08:57:19 +1100

    chore: get appveyor to publish win32 libs

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,9 +1,6 @@
-if ($APPVEYOR_REPO_TAG) {
+
   Write-Output "Running deploy (APPVEYOR_REPO_TAG) is $APPVEYOR_REPO_TAG"
   npm install node-pre-gyp node-pre-gyp-github
   npm run build:v3
   Remove-Item 'native\target' -Recurse -Force
   npm run upload-binary
-} else {
-  Write-Output "Skipping deploy (APPVEYOR_REPO_TAG) is $APPVEYOR_REPO_TAG"
-}
