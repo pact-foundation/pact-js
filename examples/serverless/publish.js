ceab0de9ec7526f7ee7caecaab73566bab998398
Beth Skurrie <beth@bethesque.com>
2020-04-23 13:11:45 +1000

    style: f u checkstyle

--- examples/serverless/publish.js
+++ examples/serverless/publish.js
@@ -1,25 +1,30 @@
-if (process.env.CI !== 'true') {
-  console.log("skipping Pact publish as not on CI...");
+if (process.env.CI !== "true") {
+  console.log("skipping Pact publish as not on CI...")
   process.exit(0)
 }
 
-const pact = require('@pact-foundation/pact-node');
-const childProcess = require('child_process')
+const pact = require("@pact-foundation/pact-node")
+const childProcess = require("child_process")
 
-const exec = command => childProcess.execSync(command).toString().trim();
+const exec = command =>
+  childProcess
+    .execSync(command)
+    .toString()
+    .trim()
 
 //Usually, you would just use the CI env vars, but to allow these examples to run from
 //local development machines, we'll fall back to the git command when the env vars aren't set.
-const gitSha = process.env.TRAVIS_COMMIT || exec('git rev-parse HEAD')
-const branch = process.env.TRAVIS_BRANCH || exec('git rev-parse --abbrev-ref HEAD')
+const gitSha = process.env.TRAVIS_COMMIT || exec("git rev-parse HEAD")
+const branch =
+  process.env.TRAVIS_BRANCH || exec("git rev-parse --abbrev-ref HEAD")
 
 const opts = {
-  pactFilesOrDirs: ['./pacts/'],
+  pactFilesOrDirs: ["./pacts/"],
   pactBroker: "https://test.pact.dius.com.au",
   pactBrokerUsername: "dXfltyFMgNOFZAxr8io9wJ37iUpY42M",
   pactBrokerPassword: "O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1",
   consumerVersion: gitSha,
-  tags: [branch]
+  tags: [branch],
 }
 
 pact
