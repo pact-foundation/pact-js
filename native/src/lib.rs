ea7b823a045bf96c30e5e76561eafe4c81481f07
Ronald Holshausen <rholshausen@dius.com.au>
2020-05-12 12:19:03 +1000

    refactor: moved the body processing functions to pact_mock_server_ffi crate

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -8,16 +8,15 @@
 use neon::prelude::*;
 use pact_matching::models::*;
 use pact_matching::models::provider_states::ProviderState;
-use pact_matching::models::json_utils::json_to_string;
 use pact_matching::models::matchingrules::{MatchingRules, MatchingRule, Category, RuleLogic};
 use pact_matching::models::generators::{Generators, GeneratorCategory, Generator};
 use pact_matching::time_utils::generate_string;
 use pact_mock_server::server_manager::ServerManager;
+use pact_mock_server_ffi::bodies::process_json;
 use env_logger::{Builder, Target};
 use uuid::Uuid;
 use std::sync::Mutex;
 use serde_json::Value;
-use serde_json::map::Map;
 use log::*;
 
 mod verify;
@@ -36,79 +35,6 @@ fn init(mut cx: FunctionContext) -> JsResult<JsString> {
     debug!("Initialising Pact native library version {}", env!("CARGO_PKG_VERSION"));
 
     Ok(cx.string(env!("CARGO_PKG_VERSION")))
-}
-
-#[deprecated(
-  since = "0.0.2",
-  note = "Moved to Pact-Rust FFI crate"
-)]
-fn process_array(array: &Vec<Value>, matching_rules: &mut Category, generators: &mut Generators, path: &String, type_matcher: bool) -> Value {
-  Value::Array(array.iter().enumerate().map(|(index, val)| {
-    let updated_path = if type_matcher {
-      path.to_owned() + "[*]"
-    } else {
-      path.to_owned() + "[" + &index.to_string() + "]"
-    };
-    match val {
-      Value::Object(ref map) => process_object(map, matching_rules, generators, &updated_path, false),
-      Value::Array(ref array) => process_array(array, matching_rules, generators, &updated_path, false),
-      _ => val.clone()
-    }
-  }).collect())
-}
-
-#[deprecated(
-  since = "0.0.2",
-  note = "Moved to Pact-Rust FFI crate"
-)]
-fn process_object(obj: &Map<String, Value>, matching_rules: &mut Category, generators: &mut Generators, path: &String, type_matcher: bool) -> Value {
-  if obj.contains_key("pact:matcher:type") {
-    if let Some(rule) = MatchingRule::from_integration_json(obj) {
-      matching_rules.add_rule(path, rule, &RuleLogic::And);
-    }
-    if let Some(gen) = obj.get("pact:generator:type") {
-      match Generator::from_map(&json_to_string(gen), obj) {
-        Some(generator) => generators.add_generator_with_subcategory(&GeneratorCategory::BODY, path, generator),
-        _ => ()
-      };
-    }
-    match obj.get("value") {
-      Some(val) => match val {
-        Value::Object(ref map) => process_object(map, matching_rules, generators, path, true),
-        Value::Array(array) => process_array(array, matching_rules, generators, path, true),
-        _ => val.clone()
-      },
-      None => Value::Null
-    }
-  } else {
-    Value::Object(obj.iter().map(|(key, val)| {
-      let updated_path = if type_matcher {
-        path.to_owned() + ".*"
-      } else {
-        path.to_owned() + "." + key
-      };
-      (key.clone(), match val {
-        Value::Object(ref map) => process_object(map, matching_rules, generators, &updated_path, false),
-        Value::Array(ref array) => process_array(array, matching_rules, generators, &updated_path, false),
-        _ => val.clone()
-      })
-    }).collect())
-  }
-}
-
-#[deprecated(
-  since = "0.0.2",
-  note = "Moved to Pact-Rust FFI crate"
-)]
-fn process_json(body: String, matching_rules: &mut Category, generators: &mut Generators) -> String {
-  match serde_json::from_str(&body) {
-    Ok(json) => match json {
-      Value::Object(ref map) => process_object(map, matching_rules, generators, &"$".to_string(), false).to_string(),
-      Value::Array(ref array) => process_array(array, matching_rules, generators, &"$".to_string(), false).to_string(),
-      _ => body
-    },
-    Err(_) => body
-  }
 }
 
 fn process_xml(body: String, matching_rules: &mut Category, generators: &mut Generators) -> Result<Vec<u8>, String> {
