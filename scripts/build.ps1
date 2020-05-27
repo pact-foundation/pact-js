68f8d302cf5f85f184c308c1ef1931367e480a86
Ronald Holshausen <rholshausen@dius.com.au>
2020-05-27 11:06:22 +1000

    chore: enable E2E tests on appveyor

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -9,35 +9,35 @@ pushd dist
 npm link
 popd
 
-# Get-ChildItem ".\examples" -Directory | ForEach-Object {
-#   if ($_.Name -ne "v3") {
-#     Write-Output "Running examples in $($_.Name)"
-#     pushd $_.FullName
-#     npm link @pact-foundation/pact
-#     npm i
-#     npm t
-#     if ($LastExitCode -ne 0) {
-#       Write-Output "Non-zero exit code!"
-#       $host.SetShouldExit($LastExitCode)
-#     }
-#     popd
-#   }
-# }
+Get-ChildItem ".\examples" -Directory | ForEach-Object {
+  if ($_.Name -ne "v3") {
+    Write-Output "Running examples in $($_.Name)"
+    pushd $_.FullName
+    npm link @pact-foundation/pact
+    npm i
+    npm t
+    if ($LastExitCode -ne 0) {
+      Write-Output "Non-zero exit code!"
+      $host.SetShouldExit($LastExitCode)
+    }
+    popd
+  }
+}
 
-# Write-Output "Done with E2E tests"
+Write-Output "Done with E2E tests"
 
-# Get-ChildItem ".\examples\v3" -Directory | ForEach-Object {
-#   Write-Output "Running V3 examples in $($_.Name)"
-#   pushd $_.FullName
-#   npm i
-#   Remove-Item -LiteralPath "node_modules\@pact-foundation\pact" -Force -Recurse
-#   npm link @pact-foundation/pact
-#   npm t
-#   if ($LastExitCode -ne 0) {
-#     Write-Output "Non-zero exit code!"
-#     $host.SetShouldExit($LastExitCode)
-#   }
-#   popd
-# }
+Get-ChildItem ".\examples\v3" -Directory | ForEach-Object {
+  Write-Output "Running V3 examples in $($_.Name)"
+  pushd $_.FullName
+  npm i
+  Remove-Item -LiteralPath "node_modules\@pact-foundation\pact" -Force -Recurse
+  npm link @pact-foundation/pact
+  npm t
+  if ($LastExitCode -ne 0) {
+    Write-Output "Non-zero exit code!"
+    $host.SetShouldExit($LastExitCode)
+  }
+  popd
+}
 
-# Write-Output "Done with V3 E2E tests"
+Write-Output "Done with V3 E2E tests"
