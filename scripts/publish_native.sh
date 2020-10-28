d5390c2ea38cf995f6038f5c6ac09ef1a2f7fa84
Ronald Holshausen <rholshausen@dius.com.au>
2020-10-28 17:13:24 +1100

    chore: update rust when building the lib

--- scripts/publish_native.sh
+++ scripts/publish_native.sh
@@ -6,6 +6,8 @@ VERSION=$(grep '\"version\"' package.json | grep -E -o "([0-9\.]+(-[a-z\.0-9]+)?
 NODE_VERSION=$(node --version)
 echo "--> Releasing native library for version ${VERSION} and Node version ${NODE_VERSION}"
 
+rustup update
+
 npm install node-pre-gyp
 npm run build:v3
 rm -rf native/target
