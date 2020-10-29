e32d2218f0ab131346790511502f1a0f91bf3696
Ronald Holshausen <rholshausen@dius.com.au>
2020-10-29 14:43:38 +1100

    chore: docker container in GH actions seems to need the default rust toolchain set

--- native/build-native.sh
+++ native/build-native.sh
@@ -6,6 +6,9 @@ VERSION=$(grep '\"version\"' package.json | grep -E -o "([0-9\.]+(-[a-z\.0-9]+)?
 NODE_VERSION=$(node --version)
 echo "--> Releasing native library for version ${VERSION} and Node version ${NODE_VERSION}"
 
+rustup install stable
+rustup default stable
+
 npm install
 npm run build:v3
 rm -rf native/target
