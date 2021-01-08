7c2a36e459fb103e368b0979761357cb8462944b
Timothy Jones <timothy.l.jones@gmail.com>
2021-01-08 16:37:53 +1100

    chore(workflow): Correct location of prepare-release script

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 npm ci
 npm run dist
 
-${DIR}/prepare.sh
+${DIR}/prepare-release.sh
 
 # Link the build so that the examples are always testing the
 # current build, in it's properly exported format
