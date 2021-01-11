1d118f1a850af17b83c3d9aa111a3760dfd85604
Ronald Holshausen <rholshausen@dius.com.au>
2021-01-11 14:02:11 +1100

    chore: build script had a travis env var

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -31,7 +31,7 @@ done
 echo "--> Running coverage checks"
 npm run coverage
 
-echo "Running V3 e2e examples build for node version ${TRAVIS_NODE_VERSION}"
+echo "Running V3 e2e examples build"
 
 docker pull pactfoundation/pact-broker
 BROKER_ID=$(docker run -e PACT_BROKER_DATABASE_ADAPTER=sqlite -d -p 9292:9292 pactfoundation/pact-broker)
