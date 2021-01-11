29f9171a115c7f2c968409ed3e7121a25bc6487b
Ronald Holshausen <rholshausen@dius.com.au>
2021-01-11 15:29:32 +1100

    chore: debug failing tests on CI

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -42,6 +42,7 @@ BROKER_ID=$(docker run -e PACT_BROKER_DATABASE_ADAPTER=sqlite -d -p 9292:9292 pa
 
 trap "docker kill $BROKER_ID" EXIT
 
+export LOG_LEVEL=debug
 for i in examples/v3/*; do
   [ -e "$i" ] || continue # prevent failure if there are no examples
   echo "------------------------------------------------"
