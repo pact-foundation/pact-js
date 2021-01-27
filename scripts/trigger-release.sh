2513d2385b7effd82e7f7df9631a18a4a359a3bd
Matt Fellows <matt.fellows@onegeek.com.au>
2021-01-27 17:42:58 +1100

    chore: fix release script to handle SSH and HTTPS git remotes

--- scripts/trigger-release.sh
+++ scripts/trigger-release.sh
@@ -3,12 +3,12 @@
 # Script to trigger release of the repository
 # Requires a Github API token with repo scope stored in the
 # environment variable GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES
-# Borrowed from Beth Skurrie's excellent script at 
+# Borrowed from Beth Skurrie's excellent script at
 # https://github.com/pact-foundation/pact-ruby/blob/master/script/trigger-release.sh
 
 : "${GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES:?Please set environment variable GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES}"
 
-repository_slug=$(git remote get-url $(git remote show) | cut -d':' -f2 | sed 's/\.git//')
+repository_slug=$(git remote get-url $(git remote show) | cut -d':' -f2 | sed 's/\.git//' | sed 's://::' | sed 's:github.com/::')
 
 output=$(curl -v -X POST https://api.github.com/repos/${repository_slug}/dispatches \
       -H 'Accept: application/vnd.github.everest-preview+json' \
