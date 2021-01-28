f052db61e2a7523ca35cdf62d1d7ed5990819d51
Timothy Jones <timothy.l.jones@gmail.com>
2021-01-28 20:31:08 +1100

    chore(ci): Ensure that windows does npm link when running examples

--- scripts/ci/build-and-test.ps1
+++ scripts/ci/build-and-test.ps1
@@ -1,10 +1,15 @@
 npm ci
 npm run dist
 
+Push-Location dist
+npm link
+Pop-Location
+
 Get-ChildItem ".\examples" -Directory | ForEach-Object {
   Write-Output "Running examples in $($_.FullName)"
   cd $_.FullName
   npm i
+  npm link @pact-foundation/pact
   npm t
   if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 }
