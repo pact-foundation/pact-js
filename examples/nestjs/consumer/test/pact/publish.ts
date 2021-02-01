e1e1ae6eb5ed04773a797a4f8559cadd12e61951
Timothy Jones <timothy.l.jones@gmail.com>
2021-02-01 14:55:14 +1100

    chore(nest-example): Don't run pact publish when not on CI

--- examples/nestjs/consumer/test/pact/publish.ts
+++ examples/nestjs/consumer/test/pact/publish.ts
@@ -2,17 +2,17 @@ import { NestFactory } from "@nestjs/core"
 import { Publisher } from "@pact-foundation/pact"
 import { PactModuleProviders } from "nestjs-pact"
 import { PactModule } from "./pact.module"
-; (async () => {
+;(async () => {
   const app = await NestFactory.createApplicationContext(PactModule)
 
   const publisher: Publisher = app.get(PactModuleProviders.PactPublisher)
   const a = 1
   // const logger: LoggerService = app.get(Logger);
 
-  // if (process.env.CI !== 'true') {
-  //   console.log('Skipping Pact publish as not on CI');
-  //   process.exit(0);
-  // }
+  if (process.env.CI !== "true") {
+    console.log("Skipping Pact publish as not on CI")
+    process.exit(0)
+  }
 
   try {
     await publisher.publishPacts()
