cc5906b415cc67f0740f55224e37eefecd322c4b
Timothy Jones <timothy.l.jones@gmail.com>
2021-02-01 15:05:52 +1100

    chore(nest-example): Inline publish config and remove module

--- examples/nestjs/consumer/test/pact/publish.ts
+++ examples/nestjs/consumer/test/pact/publish.ts
@@ -1,29 +1,42 @@
-import { NestFactory } from "@nestjs/core"
 import { Publisher } from "@pact-foundation/pact"
-import { PactModuleProviders } from "nestjs-pact"
-import { PactModule } from "./pact.module"
-;(async () => {
-  const app = await NestFactory.createApplicationContext(PactModule)
+import { resolve } from "path"
+import { execSync } from "child_process"
 
-  const publisher: Publisher = app.get(PactModuleProviders.PactPublisher)
-  const a = 1
-  // const logger: LoggerService = app.get(Logger);
+const exec = command =>
+  execSync(command)
+    .toString()
+    .trim()
 
-  if (process.env.CI !== "true") {
-    console.log("Skipping Pact publish as not on CI")
-    process.exit(0)
-  }
+if (process.env.CI !== "true") {
+  console.log("Skipping Pact publish as not on CI")
+  process.exit(0)
+}
 
-  try {
-    await publisher.publishPacts()
+// Usually, you would just use the CI env vars, but to allow these examples to run from
+// local development machines, we'll fall back to the git command when the env vars aren't set.
+const gitSha = process.env.TRAVIS_COMMIT || exec("git rev-parse HEAD")
+const branch =
+  process.env.TRAVIS_BRANCH || exec("git rev-parse --abbrev-ref HEAD")
 
-    console.log("Pact contract publishing complete!")
-    console.log("")
-    console.log("Head over to https://test.pact.dius.com.au/ and login with")
-    console.log("=> Username: dXfltyFMgNOFZAxr8io9wJ37iUpY42M")
-    console.log("=> Password: O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1")
-    console.log("to see your published contracts.")
-  } catch (e) {
-    console.error("Pact contract publishing failed: ", e)
-  }
-})()
+new Publisher({
+  pactFilesOrDirs: [resolve(process.cwd(), "pact/pacts")],
+  pactBroker: "https://test.pact.dius.com.au",
+  pactBrokerUsername: "dXfltyFMgNOFZAxr8io9wJ37iUpY42M",
+  pactBrokerPassword: "O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1",
+  consumerVersion: gitSha as string,
+  tags: [(branch as string) || "master"],
+})
+  .publishPacts()
+  .then(
+    () => {
+      console.log("Pact contract publishing complete!")
+      console.log("")
+      console.log("Head over to https://test.pact.dius.com.au/ and login with")
+      console.log("=> Username: dXfltyFMgNOFZAxr8io9wJ37iUpY42M")
+      console.log("=> Password: O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1")
+      console.log("to see your published contracts.")
+    },
+    e => {
+      console.error("Pact contract publishing failed: ", e)
+    }
+  )
