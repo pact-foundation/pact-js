c670ddea2c67bfcd6acfc27617cf0ad7ad1d0e39
Timothy Jones <timothy.l.jones@gmail.com>
2021-02-20 17:21:07 +1100

    chore(github-actions): Since removing the workaround works now, also remove the script

--- scripts/ci/lib/modify-permissions-github-actions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-
-# This script is a workaround for Ruby complaining about world writeable directories
-# and failing the tests because it writes to standard error.
-
-echo "Reducing permissions on github actions' folders to avoid ruby warnings"
-sudo chmod 755 -R /home
-sudo chmod 755 -R /usr/share
-sudo chmod 755 -R /opt
