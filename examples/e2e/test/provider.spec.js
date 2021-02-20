587e2737374e412ea699d866a9465b2cd85f25a8
Timothy Jones <timothy.l.jones@gmail.com>
2021-02-20 16:11:48 +1100

    chore(e2e-example): Use absolute-version for provider version too

--- examples/e2e/test/provider.spec.js
+++ examples/e2e/test/provider.spec.js
@@ -1,4 +1,5 @@
 const { Verifier } = require("@pact-foundation/pact")
+const { versionFromGitTag } = require("@pact-foundation/absolute-version")
 const chai = require("chai")
 const chaiAsPromised = require("chai-as-promised")
 chai.use(chaiAsPromised)
@@ -90,7 +91,10 @@ describe("Pact Verification", () => {
       // You can obtain the token from https://<your broker>.pact.dius.com.au/settings/api-tokens
       // pactBrokerToken: "<insert your token here"
       publishVerificationResult: true,
-      providerVersion: "1.0.0",
+      // Your version numbers need to be unique for every different version of your provider
+      // see https://docs.pact.io/getting_started/versioning_in_the_pact_broker/ for details.
+      // If you use git tags, then you can use @pact-foundation/absolute-version as we do here.
+      providerVersion: versionFromGitTag(),
     }
 
     return new Verifier(opts).verifyProvider().then(output => {
