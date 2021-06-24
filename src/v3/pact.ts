0cd8b0a84bd33492984cebe84ea09fd82d8d1c1e
Matt Fellows <matt.fellows@onegeek.com.au>
2021-06-24 10:57:57 +1000

    chore: fix lint from PR #700

--- src/v3/pact.ts
+++ src/v3/pact.ts
@@ -104,9 +104,7 @@ function displayRequest(request: MismatchRequest, indent: string): string {
   if (request.body) {
     const body = JSON.stringify(request.body);
     output.push(
-      `${indent}Body: ${body.substr(0, 20)}... (${
-        body.length
-      } length)`
+      `${indent}Body: ${body.substr(0, 20)}... (${body.length} length)`
     );
   }
 
