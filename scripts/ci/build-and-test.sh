08b7c74431f07a3e6ec03d8d33cc75e4a307a27b
Timothy Jones <timothy.l.jones@gmail.com>
2021-09-08 22:11:47 +1000

    build: don't run docker during tests any more

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -37,10 +37,13 @@ npm run coverage
 
 echo "Running V3 e2e examples build"
 
-docker pull pactfoundation/pact-broker
-BROKER_ID=$(docker run -e PACT_BROKER_DATABASE_ADAPTER=sqlite -d -p 9292:9292 pactfoundation/pact-broker)
+# Commented because:
+#    1. We can't run the broker on windows CI
+#    2. We use the live broker in the v3 examples now anyway
+# docker pull pactfoundation/pact-broker
+# BROKER_ID=$(docker run -e PACT_BROKER_DATABASE_ADAPTER=sqlite -d -p 9292:9292 pactfoundation/pact-broker)
 
-trap "docker kill $BROKER_ID" EXIT
+# trap "docker kill $BROKER_ID" EXIT
 
 for i in examples/v3/*; do
   [ -d "$i" ] || continue # prevent failure if not a directory
