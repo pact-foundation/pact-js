56efeb38b6ae5dc21020b9792a9d22373aa99b68
Timothy Jones <timothy.l.jones@gmail.com>
2022-12-05 23:58:35 +1100

    fix: Remove unnecessary dependency @types/bluebird (also removed bluebird internally)

--- src/common/net.ts
+++ src/common/net.ts
@@ -5,7 +5,6 @@
  */
 
 import * as net from 'net';
-import { Promise as bluebird } from 'bluebird';
 
 export const localAddresses = ['127.0.0.1', 'localhost', '0.0.0.0', '::1'];
 
@@ -27,28 +26,19 @@ export const portCheck = (port: number, host: string): Promise<void> =>
   });
 
 export const isPortAvailable = (port: number, host: string): Promise<void> =>
-  Promise.resolve(
-    bluebird
-      .map(
-        localAddresses,
-        // we meed to wrap the built-in Promise with bluebird.reflect() so we can
-        // test the result of the promise without failing bluebird.map()
-        (h) => bluebird.resolve(portCheck(port, h)).reflect(),
-        // do each port check sequentially (as localhost & 127.0.0.1 will conflict on most default environments)
-        { concurrency: 1 }
-      )
-      .then((inspections) => {
-        // if every port check failed, then fail the `isPortAvailable` check
-        if (inspections.every((inspection) => !inspection.isFulfilled())) {
-          return Promise.reject(
-            new Error(`Cannot open port ${port} on ipv4 or ipv6 interfaces`)
-          );
-        }
+  Promise.allSettled(
+    localAddresses.map((localHost) => portCheck(port, localHost))
+  ).then((settledPortChecks) => {
+    // if every port check failed, then fail the `isPortAvailable` check
+    if (settledPortChecks.every((result) => result.status === 'rejected')) {
+      return Promise.reject(
+        new Error(`Cannot open port ${port} on ipv4 or ipv6 interfaces`)
+      );
+    }
 
-        // the local addresses passed - now check the host that the user has specified
-        return portCheck(port, host);
-      })
-  );
+    // the local addresses passed - now check the host that the user has specified
+    return portCheck(port, host);
+  });
 
 export const freePort = (): Promise<number> => {
   return new Promise((res) => {
