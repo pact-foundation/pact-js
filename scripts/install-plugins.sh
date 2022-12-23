5effddb5d5a22e09717eb79a6d12ed54a78c3b3d
Timothy Jones <timothy.l.jones@gmail.com>
2022-12-24 10:54:10 +1300

    build: Add additional debugging output when plugin release API response can't be read

--- scripts/install-plugins.sh
+++ scripts/install-plugins.sh
@@ -32,7 +32,20 @@ function detect_osarch() {
     esac
 }
 
-TAG=$(curl -s https://api.github.com/repos/pact-foundation/pact-plugins/releases | jq '.[0].tag_name')
+
+echo "---  🐿 Downloading release metadata from pact-foundation/pact-plugins"
+RELEASE_API_RESPONSE=$(curl -sS https://api.github.com/repos/pact-foundation/pact-plugins/releases)
+TAG=$(echo "$RELEASE_API_RESPONSE" | jq '.[0]?.tag_name')
+if [ -z "${TAG:-}" ]; then
+    echo "--- ❌ FAILED TO GET TAG ❌"
+    echo "    Release API responded with"
+    echo ""
+    echo "$RELEASE_API_RESPONSE"
+
+    echo "   ❌ Release API response was not in expected format"
+fi
+
+
 VERSION=$(echo "$TAG" | tr -d '[:alpha:]-"')
 detect_osarch
 
