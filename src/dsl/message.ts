pact-foundation/pact-js

commit 33c145a908916ac3a83f0eec30f15b782b3a2e71

fix: Add support for state params to MessageProviderPact (#372) (#882)

src/dsl/message.ts
@@ -12,19 +12,29 @@ export interface Metadata {
 }
 
 /**
+ * Defines a state a provider must be in.
+ */
+export interface ProviderState {
+  name: string;
+  params?: {
+    [name: string]: string;
+  };
+}
+
+/**
  * A Message is an asynchronous Interaction, sent via a Provider
  *
  * @module Message
  */
 export interface Message {
-  providerStates?: [{ name: string }];
+  providerStates?: ProviderState[];
   description?: string;
   metadata?: Metadata;
   contents: AnyTemplate;
 }
 
 export interface ConcreteMessage {
-  providerStates?: [{ name: string }];
+  providerStates?: ProviderState[];
   description?: string;
   metadata?: Metadata;
   contents: AnyJson;
@@ -36,7 +46,7 @@ export interface ConcreteMessage {
  * @module Message
  */
 export interface MessageDescriptor {
-  providerStates?: [{ name: string }];
+  providerStates?: ProviderState[];
   description: string;
   metadata?: Metadata;
 }
@@ -76,5 +86,8 @@ export interface MessageProviders {
 }
 
 export interface StateHandlers {
-  [name: string]: (state: string) => Promise<unknown>;
+  [name: string]: (
+    state: string,
+    params?: { [name: string]: string }
+  ) => Promise<unknown>;
 }

src/messageProviderPact.spec.ts
@@ -48,6 +48,8 @@ describe('MesageProvider', () => {
       provider: 'myprovider',
       stateHandlers: {
         'some state': () => Promise.resolve('yay'),
+        'some state with params': (name, params) =>
+          Promise.resolve(`name: ${name}, params: ${JSON.stringify(params)}`),
       },
     });
   });
@@ -122,10 +124,22 @@ describe('MesageProvider', () => {
   describe('#setupStates', () => {
     describe('when given a handler that exists', () => {
       it('returns values of all resolved handlers', () => {
-        const findStateHandler = (provider as any).setupStates.bind(provider);
+        const setupStates = (provider as any).setupStates.bind(provider);
+        return expect(setupStates(successfulMessage)).to.eventually.deep.equal([
+          'yay',
+        ]);
+      });
+      it('passes params to the handler', () => {
+        const setupStates = (provider as any).setupStates.bind(provider);
         return expect(
-          findStateHandler(successfulMessage)
-        ).to.eventually.deep.equal(['yay']);
+          setupStates({
+            providerStates: [
+              { name: 'some state with params', params: { foo: 'bar' } },
+            ],
+          })
+        ).to.eventually.deep.equal([
+          'name: some state with params, params: {"foo":"bar"}',
+        ]);
       });
     });
     describe('when given a state that does not have a handler', () => {

src/messageProviderPact.ts
@@ -166,7 +166,7 @@ export class MessageProviderPact {
           : null;
 
         if (handler) {
-          promises.push(handler(state.name));
+          promises.push(handler(state.name, state.params));
         } else {
           logger.warn(`no state handler found for "${state.name}", ignoring`);
         }

commit 9be81ce301a324a7b13d0374851e9903556b4cd6

fix!: Rename the message-pact state handler type to `MessageStateHandlers` from `StateHandlers` avoiding the conflicting type problem introduced in #882. Fixes #1057

src/dsl/message.ts
@@ -85,7 +85,7 @@ export interface MessageProviders {
   [name: string]: MessageProvider;
 }
 
-export interface StateHandlers {
+export interface MessageStateHandlers {
   [name: string]: (
     state: string,
     params?: { [name: string]: string }

src/dsl/options.ts
@@ -4,7 +4,7 @@
  */
 import { VerifierOptions as PactCoreVerifierOptions } from '@pact-foundation/pact-core';
 import { PactfileWriteMode } from './mockService';
-import { MessageProviders, StateHandlers } from './message';
+import { MessageProviders, MessageStateHandlers } from './message';
 
 export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error';
 
@@ -69,7 +69,7 @@ export interface MessageProviderOptions {
   messageProviders: MessageProviders;
 
   // Prepare any provider states
-  stateHandlers?: StateHandlers;
+  stateHandlers?: MessageStateHandlers;
 }
 
 type ExcludedPactNodeVerifierKeys = Exclude<