3b1d92caf72d509449e7862f78a3272e5b5a521d
Yousaf Nabi <yousaf@pactflow.io>
2024-01-18 18:31:46 +0000

    chore(test): split jest test and set mode to merge

--- examples/jest/__tests__/index.spec.js
+++ examples/jest/__tests__/index.spec.js
@@ -4,13 +4,14 @@ const { pactWith } = require('jest-pact');
 const { Matchers } = require('@pact-foundation/pact');
 
 const { getMeDogs, getMeCats } = require('../index');
-const LOG_LEVEL = process.env.LOG_LEVEL || 'TRACE';
+const LOG_LEVEL = process.env.LOG_LEVEL || 'INFO';
 
 pactWith(
   {
     consumer: 'Jest-Consumer-Example',
     provider: 'Jest-Provider-Example',
     logLevel: LOG_LEVEL,
+    pactfileWriteMode: 'merge',
   },
   (provider) => {
     describe('Dogs API', () => {
@@ -58,6 +59,16 @@ pactWith(
       });
     });
 
+  }
+);
+pactWith(
+  {
+    consumer: 'Jest-Consumer-Example',
+    provider: 'Jest-Provider-Example',
+    logLevel: LOG_LEVEL,
+    pactfileWriteMode: 'merge',
+  },
+  (provider) => {
     describe('Cats API', () => {
       const CATS_DATA = [{ cat: 2 }, { cat: 3 }];
 
