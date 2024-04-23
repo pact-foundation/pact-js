560daa64635e6a72a950c08be4e1cfaf93d8aa75
mefellows <mefellows@users.noreply.github.com>
2024-04-24 09:56:23 +1000

    chore: remove unused deps: mockery and superagent

--- test/helper.ts
+++ test/helper.ts
@@ -1,10 +1,6 @@
 import pact from '@pact-foundation/pact-core';
-import * as mockery from 'mockery';
 
 // used to kill any left over mock server instances
 process.on('SIGINT', () => {
   pact.removeAllServers();
 });
-
-mockery.enable();
-mockery.warnOnUnregistered(false);
