aea1560472058881d245b463d506d8b94b904ad2
Yousaf Nabi <yousaf@pactflow.io>
2024-06-24 13:45:49 +0100

    chore: rm --ignore-scripts

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -6,7 +6,7 @@ set -x
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 . "$SCRIPT_DIR"/lib/robust-bash.sh
 
-npm ci --ignore-scripts
+npm ci
 
 npm run dist
 cp package.json ./dist
