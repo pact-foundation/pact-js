246cb688de0c8fdcb6d3fe9c96705be1f07e8cb8
JP-Ellis <josh@jpellis.me>
2024-12-06 12:38:01 +1100

    style: prettier
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/v3/xml/xmlText.ts
+++ src/v3/xml/xmlText.ts
@@ -2,7 +2,10 @@ import { Matcher } from '../types';
 import { XmlNode } from './xmlNode';
 
 export class XmlText extends XmlNode {
-  constructor(public content: string, public matcher?: Matcher<string>) {
+  constructor(
+    public content: string,
+    public matcher?: Matcher<string>
+  ) {
     super();
   }
 }
