74bc6144f7cc41a58228f6adf1eaf683364fa7a3
Yousaf Nabi <yousaf@pactflow.io>
2025-08-21 10:43:34 +0100

    chore(test): migrate base examples to v2 folder

---
+++ examples/v3/graphql/src/consumer.ts
@@ -0,0 +1,30 @@
+import { ApolloClient } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
+import { createHttpLink } from 'apollo-link-http';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: createHttpLink({
+    fetch: require('node-fetch'),
+    headers: {
+      foo: 'bar',
+    },
+    uri: 'http://127.0.0.1:4000/graphql',
+  }),
+});
+
+export function query(): any {
+  return client
+    .query({
+      query: gql`
+        query HelloQuery {
+          hello
+        }
+      `,
+      variables: {
+        foo: 'bar',
+      },
+    })
+    .then((result: any) => result.data);
+}
