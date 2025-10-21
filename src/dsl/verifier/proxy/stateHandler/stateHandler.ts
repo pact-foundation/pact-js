a323814eaa0f5133b1d0eb68c41e601fe6141de3
Matt Fellows <53900+mefellows@users.noreply.github.com>
2025-10-21 22:17:58 +1100

    fix: gracefully handle state handler errors (#1585)
    Previously, when an error was thrown in a state handler
    an HTTP 500 was sent back to the core engine, making it hard
    to debug the issue from within test frameworks.
    
    This change wraps the state handlers with error
    handling, rather than propagating the error back
    to the pact core engine. Errors will be clearly printed
    to the console (and logged at ERROR level).
    
    Fixes #631
    
    Co-authored-by: mefellows <mefellows@users.noreply.github.com>

--- src/dsl/verifier/proxy/stateHandler/stateHandler.ts
+++ src/dsl/verifier/proxy/stateHandler/stateHandler.ts
@@ -1,14 +1,43 @@
 import express from 'express';
 
+import StackUtils from 'stack-utils';
+import chalk from 'chalk';
 import { ProxyOptions, ProviderState } from '../types';
 import { setupStates } from './setupStates';
 
+const cleanStack = (e: Error) => {
+  const stack = new StackUtils({
+    cwd: process.cwd(),
+    internals: StackUtils.nodeInternals(),
+  });
+
+  if (!e.stack) return '';
+  const cleanedStack = stack.clean(e.stack);
+  const lines = cleanedStack.split('\n').map((line) => line.trim());
+
+  return lines[0];
+};
+
 export const createProxyStateHandler =
   (config: ProxyOptions) =>
-  (req: express.Request, res: express.Response): Promise<express.Response> => {
-    const message: ProviderState = req.body;
+  async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<express.Response> => {
+    const state: ProviderState = req.body;
+    try {
+      const data = await setupStates(state, config);
+      return res.json(data);
+    } catch (e) {
+      const error = `\nError executing state handler for state '${state.state}' on '${state.action}'.`;
+      const errorDetails = `↳ Error details: ${e.message}`;
+      const errorSource = `↳ Error source: ${cleanStack(e)}\n`;
+      /* eslint-disable no-console */
+      console.log(chalk.red(error));
+      console.log(chalk.red(errorDetails));
+      console.log(chalk.red(errorSource));
+      /* eslint-enable */
 
-    return Promise.resolve(setupStates(message, config))
-      .then((data) => res.json(data))
-      .catch((e) => res.status(500).send(e));
+      return res.status(200).send();
+    }
   };
