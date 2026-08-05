bc5fcb226b33070e4456804d614bdc1fa9d29c48
Yousaf Nabi <yousaf@pactflow.io>
2026-08-05 16:42:37 +0100

    fix: pact-core imports now in dist not src

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,8 +1,8 @@
-import logger from '@pact-foundation/pact-core/src/logger';
+import logger from '@pact-foundation/pact-core/dist/logger';
 
 import { version } from '../../package.json';
 
-export * from '@pact-foundation/pact-core/src/logger';
+export * from '@pact-foundation/pact-core/dist/logger';
 
 const context = `pact@${version}`;
 
